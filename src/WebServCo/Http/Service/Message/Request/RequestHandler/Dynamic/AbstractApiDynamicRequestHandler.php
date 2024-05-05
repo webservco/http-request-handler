46fb76ff7ad7bcda86d1da04c00a1b431dfb7444
Radu Maier <ariademur@webserv.co>
2024-05-05 18:14:06 +0200

    Fix AbstractApiDynamicRequestHandler

--- src/WebServCo/Http/Service/Message/Request/RequestHandler/Dynamic/AbstractApiDynamicRequestHandler.php
+++ src/WebServCo/Http/Service/Message/Request/RequestHandler/Dynamic/AbstractApiDynamicRequestHandler.php
@@ -15,23 +15,23 @@ use function sprintf;
  * A custom API specific implementation of the Dynamic RequestHandler
  * Default implementation reads route from route part 2
  * This implementation reads route from both
- * route part 2 and route part 3
+ * route part 3 and route part 4
  * (route part 2 is used for the API version)
  */
 abstract class AbstractApiDynamicRequestHandler extends AbstractDynamicRequestHandler
 {
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $routePart2 = $this->requestAttributeService->getRoutePart(2, $request);
-        if ($routePart2 === null) {
-            throw new UnexpectedValueException('Route part 2 is required.');
+        $routePart3 = $this->requestAttributeService->getRoutePart(3, $request);
+        if ($routePart3 === null) {
+            throw new UnexpectedValueException('Route part 3 is required.');
         }
 
         // In this custom situation, the route contains also the API version/
         $route = sprintf(
             '%s/%s',
-            $routePart2,
-            $this->requestAttributeService->getRoutePart(3, $request) ?? '',
+            $routePart3,
+            $this->requestAttributeService->getRoutePart(4, $request) ?? '',
         );
 
         if (!array_key_exists($route, $this->routesConfiguration)) {
